87b574edaadebba947eb5ffc56be2a343a6f49da
JobGetabu <getabujob@gmail.com>
2019-09-16 16:47:45 +0300

    added PayDoneActivity

---
+++ app/src/main/java/com/job/barbershop/util/payment/PayDoneActivity.kt
@@ -0,0 +1,13 @@
+package com.job.barbershop.util.payment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.job.barbershop.R
+
+class PayDoneActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pay_done)
+    }
+}
