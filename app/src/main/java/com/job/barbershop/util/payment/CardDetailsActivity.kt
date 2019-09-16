ee5342dc35d07d57354baf6550c0b7ada397a829
JobGetabu <getabujob@gmail.com>
2019-09-16 16:43:26 +0300

    added CardDetailsActivity

---
+++ app/src/main/java/com/job/barbershop/util/payment/CardDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.job.barbershop.util.payment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.job.barbershop.R
+
+class CardDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_details)
+    }
+}
