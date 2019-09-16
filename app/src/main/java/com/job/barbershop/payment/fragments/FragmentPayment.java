40366edac6850e3755ce14e66d57956fb940c66f
JobGetabu <getabujob@gmail.com>
2019-09-16 17:41:32 +0300

    payment module design done

---
+++ app/src/main/java/com/job/barbershop/payment/fragments/FragmentPayment.java
@@ -0,0 +1,21 @@
+package com.job.barbershop.payment.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import com.job.barbershop.R;
+
+public class FragmentPayment extends Fragment {
+
+    public FragmentPayment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_payment, container, false);
+
+        return root;
+    }
+}
---
+++ app/src/main/java/com/job/barbershop/payment/fragments/FragmentShipping.java
@@ -0,0 +1,22 @@
+package com.job.barbershop.payment.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import com.job.barbershop.R;
+
+
+public class FragmentShipping extends Fragment {
+
+    public FragmentShipping() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_shipping, container, false);
+
+        return root;
+    }
+}
