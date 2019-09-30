86228ef428420f6f63006a1d8187feb74459fed7
JobGetabu <getabujob@gmail.com>
2019-09-30 13:00:53 +0300

    summary design

---
+++ app/src/main/java/com/job/barbershop/SummaryActivity.kt
@@ -0,0 +1,12 @@
+package com.job.barbershop
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SummaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_summary)
+    }
+}
