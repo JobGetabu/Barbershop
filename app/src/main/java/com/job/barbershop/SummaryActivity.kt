1bc6998783a61e62fa04c6a251165b24419330e9
JobGetabu <getabujob@gmail.com>
2019-10-02 00:04:42 +0300

    summary design

--- app/src/main/java/com/job/barbershop/SummaryActivity.kt
+++ app/src/main/java/com/job/barbershop/SummaryActivity.kt
@@ -1,12 +1,25 @@
 package com.job.barbershop
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class SummaryActivity : AppCompatActivity() {
 
+    companion object {
+        fun newIntent(context: Context): Intent =
+            Intent(context, SummaryActivity::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_summary)
+    }
+
+    fun close(v: View){
+        onBackPressed()
+        finish()
     }
 }
